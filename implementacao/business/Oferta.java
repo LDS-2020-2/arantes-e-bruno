d7fdc8f5bedc0097964308100fbe41d24d95700f
Bruno Silveira Cequeira Lima <brunobhsclima@gmail.com>
2021-08-20 19:15:39 -0300

    Create Oferta.java

---
+++ implementacao/business/Oferta.java
@@ -0,0 +1,43 @@
+package business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Oferta {
+	private static final int MAX_ALUNOS = 60;
+	private Disciplina disciplina;
+	private List<Aluno> alunos;
+	
+	public void addAluno(Aluno a) {
+		if(alunos.size() <=MAX_ALUNOS)
+			this.alunos.add(a);
+	}
+	public void removeAluno(Aluno a) {
+		this.alunos.remove(a);
+	}
+	public void ativacao() {
+		if(alunos.size() >=3) {
+			disciplina.setAtiva(true);
+		}
+		else {
+			disciplina.setAtiva(false);
+		}
+	}
+	public Disciplina getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
+	public List<Aluno> getAlunos() {
+		return alunos;
+	}
+	
+	public Oferta(Disciplina disciplina) {
+		
+		this.setDisciplina(disciplina);
+		alunos = new ArrayList<Aluno>();
+	}
+	
+	
+}
