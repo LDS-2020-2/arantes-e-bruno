5971c280a0afee1d26a660e25080981fa8c4877b
Bruno Silveira Cequeira Lima <brunobhsclima@gmail.com>
2021-08-20 18:52:31 -0300

    Create Disciplina.java

---
+++ implementacao/business/Disciplina.java
@@ -0,0 +1,42 @@
+package business;
+
+public class Disciplina {
+	private String nome;
+	private boolean ativa;
+	private String ementa;
+	private Professor professor;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public boolean isAtiva() {
+		return ativa;
+	}
+	public void setAtiva(boolean ativa) {
+		this.ativa = ativa;
+	}
+	public String getEmenta() {
+		return ementa;
+	}
+	public void setEmenta(String ementa) {
+		this.ementa = ementa;
+	}
+	public Professor getProfessor() {
+		return professor;
+	}
+	public void setProfessor(Professor professor) {
+		this.professor = professor;
+	}
+	public Disciplina(String nome, boolean ativa, String ementa, Professor professor) {
+		
+		this.setNome(nome);
+		this.setAtiva(ativa);;
+		this.setEmenta(ementa);
+		this.setProfessor(professor);
+	}
+	
+	
+
+}
