fb2aa2aa6d84d2d3990493f8f447dd825ffd6926
Gustavo Arantes <me@arantes.dev>
2021-08-29 18:34:00 -0300

    Fix sintax error

--- implementacao/business/Usuario.java
+++ implementacao/business/Usuario.java
@@ -4,47 +4,50 @@ public class Usuario {
 	private String email;
 	private String senha;
 	private String confSenha;
-	
-	public boolean trocarSenha(String nova,String antiga) {
-		boolean troca = false;
-		if(antiga == this.senha) {
-			this.setSenha(nova);
-			troca = true;
+
+	public boolean trocarSenha(String nova, String antiga) {
+		if (!this.senha.equals(antiga)) {
+			return false;
 		}
-		return troca;
+
+		this.setSenha(nova);
+		return true;
 	}
-	public boolean efetuarLogin(String email,String senha) {
-		boolean valida = false;
-		if( email == this.email && senha == this.senha) {
-			valida = true;
-		}
-		return valida;
+
+	public boolean efetuarLogin(String email, String senha) {
+		return this.email.equals(email) && this.senha.equals(senha);
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
 	public String getConfSenha() {
 		return confSenha;
 	}
+
 	public void setConfSenha(String confSenha) {
-		if(confSenha == this.senha) {
+		if (this.senha.equals(confSenha)) {
 			this.confSenha = confSenha;
-			}
+		}
 	}
+
 	public Usuario(String email, String senha, String confSenha) {
 		this.setEmail(email);
 		this.setSenha(senha);
 		this.setConfSenha(confSenha);
 	}
-	
 
 }
